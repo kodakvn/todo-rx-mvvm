ebfd10fae228d7f4190b542b90e308ef4224bde0
KODAK <linhtt.it@gmail.com>
2019-09-16 23:07:47 +0700

    add swipe actions to table view using view model

--- TodoMVVM/TodoViewModel.swift
+++ TodoMVVM/TodoViewModel.swift
@@ -8,24 +8,80 @@
 
 import Foundation
 
+protocol TodoMenuItemViewPresentable {
+    
+    var title: String? { get }
+    var backgroundColor: String? { get }
+}
+
+protocol TodoMenuItemViewDelegate: class {
+    
+    func onMenuItemSelected()
+    
+}
+
+class TodoMenuItemViewModel: TodoMenuItemViewPresentable, TodoMenuItemViewDelegate {
+    
+    var title: String?
+    var backgroundColor: String?
+    weak var parent: TodoItemDelegate?
+    
+    init(parentViewModel: TodoItemDelegate) {
+        self.parent = parentViewModel
+    }
+    
+    func onMenuItemSelected() {
+        // base class
+    }
+    
+}
+
+class RemoveMenuItemViewModel: TodoMenuItemViewModel {
+    override func onMenuItemSelected() {
+        parent?.onRemoveSelected()
+    }
+}
+
+class DoneMenuItemViewModel: TodoMenuItemViewModel {
+    override func onMenuItemSelected() {
+        parent?.onDoneSelected()
+    }
+}
+
 protocol TodoItemDelegate: class {
     func onItemSelected()
+    func onRemoveSelected()
+    func onDoneSelected()
 }
 
 protocol TodoItemPresentable {
     
     var id: String? { get }
     var textValue: String? { get }
+    var menuItems: [TodoMenuItemViewPresentable]? { get }
 }
 
 class TodoItemViewModel: TodoItemPresentable {
     
     var id: String? = "0"
     var textValue: String? = nil
+    var menuItems: [TodoMenuItemViewPresentable]? = []
+    weak var parent: TodoViewDelegate?
     
-    init(id: String, textValue: String) {
+    init(id: String, textValue: String, parentViewModel: TodoViewDelegate) {
         self.id = id
         self.textValue = textValue
+        self.parent = parentViewModel
+        
+        let remove = RemoveMenuItemViewModel(parentViewModel: self)
+        remove.title = "Remove"
+        remove.backgroundColor = "ff0000"
+        
+        let done = DoneMenuItemViewModel(parentViewModel: self)
+        done.title = "Done"
+        done.backgroundColor = "008000"
+        
+        menuItems?.append(contentsOf: [remove, done])
     }
 }
 
@@ -33,11 +89,21 @@ extension TodoItemViewModel: TodoItemDelegate {
     func onItemSelected() {
         print("select \(id)")
     }
+    
+    func onRemoveSelected() {
+        parent?.onDeleteItem(todoId: id!)
+    }
+    
+    func onDoneSelected() {
+        parent?.onDoneItem(todiId: id!)
+    }
 }
 
-protocol TodoViewDelegate {
+protocol TodoViewDelegate: class {
     
     func onAddTodoItem()
+    func onDeleteItem(todoId: String)
+    func onDoneItem(todiId: String)
 }
 
 protocol TodoViewPresentable {
@@ -54,9 +120,9 @@ class TodoViewModel: TodoViewPresentable {
     
     init(view: TodoView) {
         self.view = view
-        let item1 = TodoItemViewModel(id: "1", textValue: "Washing clothes")
-        let item2 = TodoItemViewModel(id: "2", textValue: "Buy Groceries")
-        let item3 = TodoItemViewModel(id: "3", textValue: "Wash car")
+        let item1 = TodoItemViewModel(id: "1", textValue: "Washing clothes", parentViewModel: self)
+        let item2 = TodoItemViewModel(id: "2", textValue: "Buy Groceries", parentViewModel: self)
+        let item3 = TodoItemViewModel(id: "3", textValue: "Wash car", parentViewModel: self)
         items.append(contentsOf: [item1, item2, item3])
     }
 }
@@ -67,11 +133,23 @@ extension TodoViewModel: TodoViewDelegate {
             return
         }
         let index = items.count + 1
-        let newItem = TodoItemViewModel(id: "\(index)", textValue: value)
+        let newItem = TodoItemViewModel(id: "\(index)", textValue: value, parentViewModel: self)
         items.append(newItem)
         newTodoItem = nil
         view?.insertTodoItem()
     }
     
+    func onDeleteItem(todoId: String) {
+        guard let index = items.firstIndex(where: { $0.id == todoId }) else {
+            return
+        }
+        
+        items.remove(at: index)
+        view?.removeTodoItem(at: index)
+    }
+    
+    func onDoneItem(todiId: String) {
+        
+    }
     
 }
--- TodoMVVM/ViewController.swift
+++ TodoMVVM/ViewController.swift
@@ -8,9 +8,30 @@
 
 import UIKit
 
+extension String {
+    var hexColor: UIColor {
+        let hex = trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
+        var int = UInt32()
+        Scanner(string: hex).scanHexInt32(&int)
+        let a, r, g, b: UInt32
+        switch hex.count {
+        case 3: // RGB (12-bit)
+            (a, r, g, b) = (255, (int >> 8) * 17, (int >> 4 & 0xF) * 17, (int & 0xF) * 17)
+        case 6: // RGB (24-bit)
+            (a, r, g, b) = (255, int >> 16, int >> 8 & 0xFF, int & 0xFF)
+        case 8: // ARGB (32-bit)
+            (a, r, g, b) = (int >> 24, int >> 16 & 0xFF, int >> 8 & 0xFF, int & 0xFF)
+        default:
+            return .clear
+        }
+        return UIColor(red: CGFloat(r) / 255, green: CGFloat(g) / 255, blue: CGFloat(b) / 255, alpha: CGFloat(a) / 255)
+    }
+}
+
 protocol TodoView: class {
     
     func insertTodoItem()
+    func removeTodoItem(at index: Int)
     
 }
 
@@ -40,8 +61,10 @@ class ViewController: UIViewController {
             return
         }
         
-        viewModel?.newTodoItem = value
-        viewModel?.onAddTodoItem()
+        DispatchQueue.global(qos: .userInitiated).async {
+            self.viewModel?.newTodoItem = value
+            self.viewModel?.onAddTodoItem()
+        }
     }
 }
 
@@ -70,6 +93,42 @@ extension ViewController: UITableViewDelegate {
         let item = viewModel?.items[indexPath.row]
         (item as? TodoItemDelegate)?.onItemSelected()
     }
+    
+    @available(iOS 11.0, *)
+    func tableView(_ tableView: UITableView, leadingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        
+        let item = self.viewModel?.items[indexPath.row]
+        let actions = item?.menuItems?.map({ menuItem -> UIContextualAction in
+            let action = UIContextualAction(style: .normal, title: menuItem.title) { (action, view, success: (Bool) -> Void) in
+                DispatchQueue.global(qos: .userInitiated).async {
+                    (menuItem as? TodoMenuItemViewDelegate)?.onMenuItemSelected()
+                }
+                success(true)
+            }
+            
+            action.backgroundColor = menuItem.backgroundColor?.hexColor
+            return action
+        })
+        
+        return UISwipeActionsConfiguration(actions: actions!)
+    }
+    
+    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+        
+        let item = self.viewModel?.items[indexPath.row]
+        let actions = item?.menuItems?.map({ menuItem -> UITableViewRowAction in
+            let action = UITableViewRowAction(style: .default, title: menuItem.title) { (action, indexPath) in
+                DispatchQueue.global(qos: .userInitiated).async {
+                    (menuItem as? TodoMenuItemViewDelegate)?.onMenuItemSelected()
+                }
+            }
+            
+            action.backgroundColor = menuItem.backgroundColor?.hexColor
+            return action
+        })
+        
+        return actions
+    }
 }
 
 extension ViewController: TodoView {
@@ -78,10 +137,22 @@ extension ViewController: TodoView {
             return
         }
         
-        textFieldViewItem.text = viewModel?.newTodoItem
-        tableViewItems.beginUpdates()
-        tableViewItems.insertRows(at: [IndexPath(row: items.count-1, section: 0)], with: .automatic)
-        tableViewItems.endUpdates()
+        DispatchQueue.main.async {
+            self.textFieldViewItem.text = self.viewModel?.newTodoItem
+            self.tableViewItems.beginUpdates()
+            self.tableViewItems.insertRows(at: [IndexPath(row: items.count-1, section: 0)], with: .automatic)
+            self.tableViewItems.endUpdates()
+        }
+        
+        
+    }
+    
+    func removeTodoItem(at index: Int) {
+        DispatchQueue.main.async {
+            self.tableViewItems.beginUpdates()
+            self.tableViewItems.deleteRows(at: [IndexPath(row: index, section: 0)], with: .automatic)
+            self.tableViewItems.endUpdates()
+        }
     }
 }
 
